7f0a220981be1d3ac8b97e4d58da49cf991e46be
levinhtxbt <levinhtxbt@gmail.com>
2017-02-19 10:37:46 +0700

    init project HomeCinema

---
+++ HomeCinema.Entities/IEntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCinema.Entities
+{
+    public class IEntityBase
+    {
+        int ID { get; set; }
+    }
+}
---
+++ HomeCinema/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomeCinema.Global" Language="C#" %>
